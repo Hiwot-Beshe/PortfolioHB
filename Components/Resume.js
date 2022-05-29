6b3562fbde3cf2e08d6fe88f2eafdffa8ebc211a
Hiwot-Beshe <beshehiwot@gmail.com>
2022-05-28 21:24:44 -0700

    feat(resume and work):create pages

---
+++ Components/Resume.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Image, useWindowDimensions } from "react-native";
+
+
+export default function Resume() {
+
+	const { width, height } = useWindowDimensions();
+	const widthBreakpoint = 500;
+
+	return (
+		<View >
+			
+		</View>
+	);
+}
+const styles = StyleSheet.create({
+	cent: {
+		textAlign: "center",
+		backgroundColor: "#1a1a1a",
+	},
+	
+});
---
+++ Components/Work.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View, useWindowDimensions } from "react-native";
+
+
+export default function Work() {
+
+	const { width, height } = useWindowDimensions();
+	const widthBreakpoint = 500;
+
+	return (
+		<View >
+			
+		</View>
+	);
+}
+const styles = StyleSheet.create({
+	cent: {
+		textAlign: "center",
+		backgroundColor: "#1a1a1a",
+	},
+});
