e6c41ee4a6eef421cdf6eb4c31bf7e04b254d418
Hiwot-Beshe <beshehiwot@gmail.com>
2022-05-30 09:01:39 -0700

    chore()update

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
 	entry: {
 		index: "./src/index.js",
 		another: "./src/another-module.js",
+		index: {
+			import: "./src/index.js",
+			dependOn: "shared",
+		},
+		another: {
+			import: "./src/another-module.js",
+			dependOn: "shared",
+		},
+		shared: "lodash",
 	},
 	output: {
 		filename: "[name].bundle.js",
