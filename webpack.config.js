c57e4068da4900c2c73b3622323cf94ecc14324d
Hiwot-Beshe <beshehiwot@gmail.com>
2022-05-30 08:43:09 -0700

    fix()deploy bug

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+		another: "./src/another-module.js",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	optimization: {
+		splitChunks: {
+			chunks: "all",
+		},
+	},
+	performance: {
+		hints: false,
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000,
+	},
+};
